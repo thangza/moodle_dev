cb4470d8de9aaf02c29e88a299ccc48ae4236804
edargorter <zdbowditch777@gmail.com>
2018-08-29 16:31:15 +0200

    Added tunnel.sh

---
+++ tunnel.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SSH Tunnel into "target_host" via the proxy "proxy_host"
+
+#Usage: ./tunnel.sh [dev number: 1, 2] s[student number]
+
+
+target_host_first="moodle-dev"
+target_host_second=".ms.wits.ac.za"
+proxy_host="lamp.ms.wits.ac.za"
+target_dev=$1
+username_proxy=$2
+local_port=3333
+target_port=22
+
+echo "Connecting to ${proxy_host} on local port: ${local_port}"
+echo "..."
+ssh -L ${local_port}:${target_host_first}${target_dev}${target_host_second}:${target_port} ${username_proxy}@${proxy_host}
+echo "Connection with ${proxy_host} closed."
