b26370a8398bc392e76747a84264b697b8c34558
chipman17 <rylan.perumal@gmail.com>
2018-09-19 22:11:41 +0200

    Queue Position Update

--- dev_1/witsoj/ajax.php
+++ dev_1/witsoj/ajax.php
@@ -9,22 +9,37 @@ define('ASSIGNFEEDBACK_WITSOJ_STATUS_PENDING', 0);
 define('ASSIGNFEEDBACK_WITSOJ_STATUS_JUDGING', 1);
 
 $gradeid = $_POST['sendID'];
-global $DB; 
-#$sql = "SELECT ojfeedback FROM {assignfeedback_witsoj} s ".
-#	" WHERE userid=".$id . " AND assignment = ".$assignid . 
-#	" ORDER BY timemodified ASC ";
-#$rec = $DB->get_records_sql($sql);
+global $DB;
+
+// $sql = "SELECT * FROM {assignfeedback_witsoj} s ".
+// 	" WHERE userid=".$id . " AND assignment = ".$assignid .
+// 	" ORDER BY timemodified ASC ";
+// $rec = $DB->get_records_sql($sql);
 
 $feedbackcomment = $DB->get_record('assignfeedback_witsoj', array('grade'=>$gradeid));
 $feedbackstatus = $feedbackcomment->status ;
+$assignmentid = $feedbackcomment->assignment;
+
 
 if($feedbackstatus  == ASSIGNFEEDBACK_WITSOJ_STATUS_PENDING){
-	echo "WAITING" ;
+	console.log("reached if statement");
+	$sql = " SELECT position FROM
+		(SELECT grade, @position := @position + 1 as position FROM
+		(SELECT @position := 0) A,
+		(SELECT * FROM mdl_assignfeedback_witsoj WHERE status= 0 ORDER BY timemodified ASC) B
+	    WHERE assignment= '$assignmentid') C
+	    WHERE grade = '$gradeid'";
+	$rec = $DB->get_records_sql($sql); // queries the datebase
+	foreach($rec as $position => $v){
+		echo "Your position in the queue is " .$position;
+	}
 
 }elseif ($feedbackstatus  == ASSIGNFEEDBACK_WITSOJ_STATUS_JUDGING){
-	echo "JUDGING" ;
+	console.log("reached elseif statement");
+	echo "Judging" ;
 
 }else{
+	console.log("reached else statement");
 	echo "GRADED";
 	//echo $feedbackcomment->commenttext;
 }
