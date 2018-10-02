c6de8f0445ac849f72cf5cd813d9cfadee8fb707
keartie94 <keartie94@gmail.com>
2018-10-02 20:26:03 +0200

    test unit test

--- dev_1/witsoj/unitTests/locallibTest.php
+++ dev_1/witsoj/unitTests/locallibTest.php
@@ -7,13 +7,13 @@ use PHPUnit\DbUnit\TestCaseTrait;
 #$db = new PDO($connection_string, $mysql_user, $mysql_password);
 
 class locallibTest extends TestCase{
-  
+
   public function testTrueisTrue(){
     $foo = true;
     $this->assertTrue($foo);
   }
 
-  public function testGetFeedbackWitsOJ(){
+  /*public function testGetFeedbackWitsOJ(){
     $mysql_host = getenv('MYSQL_HOST') ?: 'localhost';
     $mysql_user = getenv('MYSQL_USER') ?: 'root';
     $mysql_password = getenv('MYSQL_PASSWORD') ?: '';
@@ -27,7 +27,22 @@ class locallibTest extends TestCase{
     $expected = $stmt->fetchAll();
     $result=$tester->get_feedback_witsoj(1);
     $this->assertEquals($expected,$result,$msg);
+  }*/
+
+  public function testdbTest(){
+    $mysql_host = getenv('MYSQL_HOST') ?: 'localhost';
+    $mysql_user = getenv('MYSQL_USER') ?: 'root';
+    $mysql_password = getenv('MYSQL_PASSWORD') ?: '';
+    $connection_string = "mysql:host={$mysql_host};dbname=moodle";
+    $db = new PDO($connection_string, $mysql_user, $mysql_password);
+
+    $stmt = $db->prepare("SELECT id FROM mdl_assignfeedback_witsoj WHERE id=1");
+    $stmt->execute();
+    $result = $stmt->fetchAll();
+    $this->assertEquals(array(1),$result[0],"Yay!");
   }
+
+
 
 
 
