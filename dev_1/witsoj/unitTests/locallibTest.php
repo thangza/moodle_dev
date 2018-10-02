465296d7832a7ceee15951bbb62bb3bfb58253b0
keartie94 <keartie94@gmail.com>
2018-10-02 17:14:33 +0200

    test unit test

---
+++ dev_1/witsoj/unitTests/locallibTest.php
@@ -0,0 +1,13 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class locallibTest extends TestCase{
+  public function testTrueisTrue(){
+    $foo = true;
+    $this->assertTrue($foo);    
+  }
+
+
+
+
+}
