e0f264ee3614525539f53d67f556c2f7644eab94
keartie94 <keartie94@gmail.com>
2018-10-02 18:01:26 +0200

    test unit test

--- dev_1/witsoj/unitTests/locallibTest.php
+++ dev_1/witsoj/unitTests/locallibTest.php
@@ -7,9 +7,14 @@ class locallibTest extends TestCase{
     $this->assertTrue($foo);
   }
 
-  public function testDefine(){
-    $msg="define pending works correctly";
-    $this->assertEquals('ASSIGNFEEDBACK_WITSOJ_STATUS_PENDING',0,$msg);
+  public function test_get_feedback_witsoj(){
+    global $DB;
+    $tester=new assign_feedback_witsoj;
+    $msg="gets feedback correctly works correctly";
+    $sql="SELECT * FROM mdl_assignfeedback_witsoj WHERE id=1";
+    $expected=$DB->get_record_sql($sql);
+    $result=$tester->get_feedback_witsoj(1);
+    $this->assertEquals($expected,$result,$msg);
   }
 
 
