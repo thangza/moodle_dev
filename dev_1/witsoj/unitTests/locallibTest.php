2fff0beb68047e2ac814c08f690338466828d8ba
keartie94 <keartie94@gmail.com>
2018-10-02 20:35:57 +0200

    test unit test

--- dev_1/witsoj/unitTests/locallibTest.php
+++ dev_1/witsoj/unitTests/locallibTest.php
@@ -35,11 +35,12 @@ class locallibTest extends TestCase{
     $mysql_password = getenv('MYSQL_PASSWORD') ?: '';
     $connection_string = "mysql:host={$mysql_host};dbname=moodle";
     $db = new PDO($connection_string, $mysql_user, $mysql_password);
-
-    $stmt = $db->prepare("SELECT id FROM mdl_assignfeedback_witsoj WHERE id=1");
+    $tester=new assign_feedback_witsoj;
+    $result=$tester->get_feedback_witsoj(1);
+    $stmt = $db->prepare("SELECT * FROM mdl_assignfeedback_witsoj WHERE id=1");
     $stmt->execute();
-    $result = $stmt->fetchAll();
-    $this->assertEquals(array(1),$result[0],"Yay!");
+    $expected = $stmt->fetchAll();
+    $this->assertEquals($expected,$result,"Yay!");
   }
 
 
