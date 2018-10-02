eb24ab6eb5546f13f77a8adfcdc3853453e594e2
keartie94 <keartie94@gmail.com>
2018-10-02 17:39:46 +0200

    test unit test

--- dev_1/witsoj/unitTests/locallibTest.php
+++ dev_1/witsoj/unitTests/locallibTest.php
@@ -4,7 +4,12 @@ use PHPUnit\Framework\TestCase;
 class locallibTest extends TestCase{
   public function testTrueisTrue(){
     $foo = true;
-    $this->assertTrue($foo);    
+    $this->assertTrue($foo);
+  }
+
+  public function testDefine(){
+    $msg="define pending works correctly";
+    $this->assertEquals('ASSIGNFEEDBACK_WITSOJ_STATUS_PENDING',0,$msg);
   }
 
 
