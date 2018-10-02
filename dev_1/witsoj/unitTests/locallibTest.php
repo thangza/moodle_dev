563375b750b52d0b622d6d2ddc58dcc1693e94da
keartie94 <keartie94@gmail.com>
2018-10-02 19:07:25 +0200

    test unit test

--- dev_1/witsoj/unitTests/locallibTest.php
+++ dev_1/witsoj/unitTests/locallibTest.php
@@ -8,10 +8,10 @@ $mysql_password = getenv('MYSQL_PASSWORD') ?: '';
 
 $connection_string = "mysql:host={$mysql_host};dbname=moodle";
 
-$db = new PDO($connection_string, $mysql_user, $mysql_password);
+#$db = new PDO($connection_string, $mysql_user, $mysql_password);
 
 class locallibTest extends TestCase{
-
+  $db = new PDO($connection_string, $mysql_user, $mysql_password);
 
   public function testTrueisTrue(){
     $foo = true;
@@ -19,11 +19,10 @@ class locallibTest extends TestCase{
   }
 
   public function test_get_feedback_witsoj(){
-    global $DB = new PDO($connection_string, $mysql_user, $mysql_password);
     #$tester=new assign_feedback_witsoj;
     #$msg="gets feedback correctly works correctly";
     $sql="SELECT id FROM mdl_assignfeedback_witsoj WHERE id=1";
-    $expected=$DB->get_record_sql($sql);
+    $expected=$db->get_record_sql($sql);
     #$result=$tester->get_feedback_witsoj(1);
     $this->assertEquals($expected,1,$msg);
   }
