f4467480207647695885ff32a209541fa5aa670f
keartie94 <keartie94@gmail.com>
2018-10-02 21:40:38 +0200

    test unit test

--- dev_1/witsoj/unitTests/locallibTest.php
+++ dev_1/witsoj/unitTests/locallibTest.php
@@ -2,7 +2,6 @@
 use PHPUnit\Framework\TestCase;
 use PHPUnit\DbUnit\TestCaseTrait;
 
-require_once('moodle_dev/dev_1/witsoj/locallib.php');
 
 class locallibTest extends TestCase{
 
@@ -11,22 +10,6 @@ class locallibTest extends TestCase{
     $this->assertTrue($foo);
   }
 
-  /*public function testGetFeedbackWitsOJ(){
-    $mysql_host = getenv('MYSQL_HOST') ?: 'localhost';
-    $mysql_user = getenv('MYSQL_USER') ?: 'root';
-    $mysql_password = getenv('MYSQL_PASSWORD') ?: '';
-    $connection_string = "mysql:host={$mysql_host};dbname=moodle";
-    $db = new PDO($connection_string, $mysql_user, $mysql_password);
-    $tester=new assign_feedback_witsoj;
-    $msg="Awe";
-    $sql="SELECT id FROM mdl_assignfeedback_witsoj WHERE id=1";
-    $stmt=$db->prepare($sql);
-    $stmt->execute();
-    $expected = $stmt->fetchAll();
-    $result=$tester->get_feedback_witsoj(1);
-    $this->assertEquals($expected,$result,$msg);
-  }*/
-
   public function testdbTest(){
     #global $DB;
     $mysql_host = getenv('MYSQL_HOST') ?: 'localhost';
@@ -34,17 +17,11 @@ class locallibTest extends TestCase{
     $mysql_password = getenv('MYSQL_PASSWORD') ?: '';
     $connection_string = "mysql:host={$mysql_host};dbname=moodle";
     $db = new PDO($connection_string, $mysql_user, $mysql_password);
-    $tester=new assign_feedback_witsoj;
-    $result=$tester->get_feedback_witsoj(1);
+    $result=$db->get_record('mdl_assignfeedback_witsoj', array('grade'=>1));
     $stmt = $db->prepare("SELECT * FROM mdl_assignfeedback_witsoj WHERE id=1");
     $stmt->execute();
     $expected = $stmt->fetchAll();
     $this->assertEquals($expected,$result,"Yay!");
   }
-
-
-
-
-
 
 }
