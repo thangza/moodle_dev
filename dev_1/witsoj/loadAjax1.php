941cecb406c4a27246abe148beea5bbfcd897874
tamlinlove <tamlinlollislove@gmail.com>
2018-11-02 12:04:26 +0200

    Testing view_summary_done it should be fine

--- dev_1/witsoj/loadAjax1.php
+++ dev_1/witsoj/loadAjax1.php
@@ -1,32 +1,7 @@
 <?php
 
-function loadAjax($id){
+public function loadAjax($id){
 	return $id;
 }
 
 ?>
-/*
-setInterval(function(){
-	var id = <?php echo $id ?> ;
-	$.ajax({
-		url: 'feedback/witsoj/ajax.php',
-		type :"POST",
-		data:{sendID: id},
-		success: function(response){
-			console.log("Hello");
-			console.log(id);
-			if(response != "GRADED"){
-				$("#tmp").html(response);
-			}else{
-				location.reload() ;
-			}
-		},
-		error: function(status){
-			console.log(id);
-			alert(JSON.stringify(status,null,4)) ;
-		}
-
-	});
-
-},1000)
-*/
